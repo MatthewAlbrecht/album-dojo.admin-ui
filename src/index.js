287f6f0b8c2fde98e708702b470527ddb5937dc1
Matthew Albrecht <matthewalbrecht@matts-macbook.myfiosgateway.com>
2020-05-21 21:57:22 -0400

    apollo configuration added

--- src/index.js
+++ src/index.js
@@ -2,40 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'index.scss'
 import { Provider } from 'react-redux'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import configureStore from 'store'
-import ApolloClient, { gql } from 'apollo-boost'
-
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { CookiesProvider } from 'react-cookie'
 import App from 'components/App/App'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:2017/graphql',
+const cache = new InMemoryCache()
+cache.writeData({
+  data: {
+    isLoggedIn: false,
+    currentUser: {
+      id: '',
+      username: '',
+      points: {},
+      __typename: 'CurrentUser',
+    },
+  },
 })
 
-client
-  .query({
-    query: gql`
-      {
-        user {
-          username
-          email
-          id
-          role
-          points
-          albums {
-            id
-            albumId
-            tags
-            rating
-          }
-        }
-      }
-    `,
-  })
-  .then(result => console.log('result', result))
+const client = new ApolloClient({
+  cache,
+  uri: 'http://localhost:2017/graphql',
+  resolvers: {},
+  request: operation => {
+    const token = localStorage.getItem('token')
+    if (typeof token === 'string') {
+      //TODO: break this out into it's own utils function validateLocalStorageToken
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : '',
+        },
+      })
+    }
+  },
+})
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={configureStore()}>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 )
